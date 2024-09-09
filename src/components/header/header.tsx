f67777b015e0c43f64ae2b1767c88e30bc78fb92
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-09-09 17:31:24 +0200

    header design with hardcoded values

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { IconApps24, IconMail24, IconMessages24, LogoIconWhite } from '@dhis2/ui';
+import { Link, NavLink } from 'react-router-dom';
+import { menuItems } from '../../data/menuLinks';
+import './header.css';
+
+
 
 export default function header() {
   return (
-    <div>
-   header
-    </div>
-  )
+    <nav className='bg-dhisMainBlue text-white flex items-center justify-between '>
+      <div className='flex items-center justify-center'>
+        <div className='flex items-center justify-center'>
+          <div className='cursor-pointer hover:bg-[#1A557F] flex items-center justify-center mr-[12px] border-r-[1px] border-dhisGrey50 p-[10px]'>
+            <Link to='/'>
+              <LogoIconWhite className='h-[26px] w-[27px]' />
+            </Link>
+          </div>
+          <Link to={'/'} className='text-[14px] font-[500] border-r-[1px] border-dhisGrey50 p-[12px]'>
+            <p>Rwanda Integrated Disease Surveillance - rw-dashbaord</p>
+          </Link>
+        </div>
+        <menu>
+          {menuItems.map((menuItem, index) => (
+            <NavLink
+              to={`/${menuItem.link}`}
+              key={index}
+              className={({ isActive }) =>
+                `p-[12px] ${isActive ? 'bg-[#1A557F]' : 'hover:bg-[#1A557F]'}`
+              }
+            >
+              {menuItem.name}
+            </NavLink>
+          ))}
+        </menu>
+      </div>
+      <div>
+        <div className='flex items-center'>
+          <div className='p-[12px] cursor-pointer hover:bg-dhisDarkBlue'>
+            <Link to={`${baseUrl}/dhis-web-interpretation`}>
+              <IconMessages24 />
+              <span className='bg-dhisMainGreen rounded-full w-[18px] h-[18px] px-[4px] text-[13px] font-[600] top-[3px] absolute ml-4'>5</span>
+            </Link>
+          </div>
+          <div className='p-[12px] cursor-pointer hover:bg-dhisDarkBlue'>
+            <Link to={`${baseUrl}/dhis-web-messaging`}>
+              <IconMail24 />
+              <span className='bg-dhisMainGreen rounded-full w-[18px] h-[18px] px-[4px] text-[13px] font-[600] top-[3px] absolute ml-4'>5</span>
+            </Link>
+          </div>
+          <div className='p-[12px] cursor-pointer hover:bg-dhisDarkBlue'>
+            <IconApps24 />
+          </div>
+          <div className='p-[6px] cursor-pointer hover:bg-dhisDarkBlue'>
+            <div className='rounded-full bg-dhisGrey900 h-[36px] w-[36px] flex items-center justify-center'>DI</div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
---
+++ src/data/menuLinks.ts
@@ -0,0 +1,18 @@
+export const menuItems = [
+    {
+        name: "Dashboards",
+        link: "dashboards"
+    },
+    {
+        name: "Charts",
+        link: "charts"
+    },
+    {
+        name: "Alerts",
+        link: "alerts"
+    },
+    {
+        name: "Settings",
+        link: "settings"
+    }
+];
