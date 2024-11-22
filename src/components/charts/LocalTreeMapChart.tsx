35346ac0b24005e8c90167091702b9e875bcf890
NSENGIYUMVA-Christian <chris4dev7@gmail.com>
2024-11-22 18:04:19 +0000

    refined tree

--- src/components/charts/LocalTreeMapChart.tsx
+++ src/components/charts/LocalTreeMapChart.tsx
@@ -18,7 +18,7 @@ const CustomTooltip = ({ active, payload }: any) => {
     return (
       <div className="bg-white p-2 rounded-md shadow-md border border-gray-300">
         <p className="font-semibold">{`${data.root?.name || "Category"} - ${data.name || "Item"}`}</p>
-        <p>{`total: ${data.size || 0}`}</p>
+        <p>{`size: ${data.size || 0}`}</p>
       </div>
     );
   }
@@ -92,10 +92,13 @@ export const LocalTreeMapChart: React.FC<genericChartsProps> = ({
 };
 
 const CustomizedContent = (props: any) => {
-  const { x, y, width, height, index, depth, name, payload, colors } = props;
+  const { x, y, width, height, index, depth, name, colors, root } = props;
+
+  const centerX = x + width / 2;
+  const centerY = y + height / 2;
 
   const fillColor =
-    depth < 2 ? colors[Math.floor((index / props.root.children.length) * colors.length)] : "#ffffff00";
+    depth < 2 ? colors[Math.floor((index / root?.children.length) * colors.length)] : "#ffffff00";
 
   return (
     <g>
@@ -110,7 +113,7 @@ const CustomizedContent = (props: any) => {
         strokeOpacity={1 / (depth + 1e-10)}
       />
       {depth === 1 && (
-        <text x={x + width / 2} y={y + height / 2} textAnchor="middle" fill="#fff" fontSize={12}>
+        <text x={x + width / 2} y={y + height / 2} textAnchor="middle" fill="#6c0505" fontSize={12}>
           {name}
         </text>
       )}
@@ -119,6 +122,29 @@ const CustomizedContent = (props: any) => {
           {index + 1}
         </text>
       )}
+    
+    {depth === 2 && (
+         <text
+         x={centerX}
+         y={centerY + 10} // Adjust the vertical offset for spacing
+         textAnchor="middle"
+         dominantBaseline="middle"
+         fill="#fff"
+         fontSize={12}
+       >
+        {name} 
+        {/* {props.size}  */}
+       </text>
+      // width and height should depend on dynamic like should take size of it content
+        // <foreignObject x={x} y={y} width={"40px"} height={"40px"}   >
+        //   <div className="bg-white p-2 rounded-md shadow-md border border-gray-300 w-full h-full">
+        //     <p className="font-semibold">{`${name || "Item"}`}</p>
+        //     <p>{`size: ${props.size || 0}`}</p>
+        //   </div>
+        // </foreignObject>
+      )}
     </g>
   );
 };
+
+
