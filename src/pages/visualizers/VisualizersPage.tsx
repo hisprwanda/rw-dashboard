675133ea03dae5bdd4ffaf3720d68bbb4fea85ae
NSENGIYUMVA-Christian <chris4dev7@gmail.com>
2024-12-31 11:49:18 +0000

    added number of selects items in main dimensions

--- src/pages/visualizers/VisualizersPage.tsx
+++ src/pages/visualizers/VisualizersPage.tsx
@@ -279,27 +279,27 @@ useEffect(() => {
                         <div>
                             {/* Select Data Source Dropdown */}
                             <div className="mb-4">
-                                <label className="block text-sm font-medium text-gray-700 mb-1">Select Data source</label>
+                                <label className="block text-sm font-medium text-gray-700 mb-1">Data source</label>
                                 <select   value={selectedDataSourceOption} onChange={handleDataSourceOnChange}
          className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                                    <option value={currentInstanceId}  >{ systemInfo?.title?.applicationTitle}</option>
                                     {dataSourceOptions}
                                 </select>
                             </div>
-                            {/* Indicators */}
+                            {/* data items */}
                             <div className="mb-4">
-                                <label className="block text-sm font-medium text-gray-700 mb-1">Data</label>
-                                <Button disabled={isFetchCurrentInstanceDataItemsLoading || isFetchExternalInstanceDataItemsLoading} variant="source" text={`${(isFetchCurrentInstanceDataItemsLoading || isFetchExternalInstanceDataItemsLoading ) ? "Loading.." : "Add +"} `} onClick={handleShowDataModal} />
+                                <label className="block text-sm font-medium text-gray-700 mb-1">Main Dimensions</label>
+                                <Button disabled={isFetchCurrentInstanceDataItemsLoading || isFetchExternalInstanceDataItemsLoading} variant="source" text={`${(isFetchCurrentInstanceDataItemsLoading || isFetchExternalInstanceDataItemsLoading ) ? "Loading.." : `Data ${analyticsDimensions?.dx?.length === 0 ? "" : `(${analyticsDimensions?.dx?.length})`}`} `} onClick={handleShowDataModal} />
                             </div>
                             {/* Period */}
                             <div className="mb-4">
-                                <label className="block text-sm font-medium text-gray-700 mb-1">Period</label>
-                              <Button disabled={isFetchCurrentInstanceDataItemsLoading || isFetchExternalInstanceDataItemsLoading} variant="source"  text={`${(isFetchCurrentInstanceDataItemsLoading || isFetchExternalInstanceDataItemsLoading ) ? "Loading.." : "Add +"} `} onClick={handleShowPeriodModal} /> 
+                                {/* <label className="block text-sm font-medium text-gray-700 mb-1">Period</label> */}
+                              <Button disabled={isFetchCurrentInstanceDataItemsLoading || isFetchExternalInstanceDataItemsLoading} variant="source"  text={`${(isFetchCurrentInstanceDataItemsLoading || isFetchExternalInstanceDataItemsLoading ) ? "Loading.." : `Period ${analyticsDimensions?.pe?.length === 0 ? "" : `(${analyticsDimensions?.pe?.length})`} `} `} onClick={handleShowPeriodModal} /> 
                             </div>
                             {/* Organization Unit */}
                             <div className="mb-4">
-                                <label className="block text-sm font-medium text-gray-700 mb-1">Organisation Unit</label>
-                                <Button disabled={isFetchCurrentInstanceDataItemsLoading || isFetchExternalInstanceDataItemsLoading} variant="source"  text={`${(isFetchCurrentInstanceDataItemsLoading || isFetchExternalInstanceDataItemsLoading ) ? "Loading.." : "Add +"} `} onClick={handleShowOrganizationUnitModal} />
+                                {/* <label className="block text-sm font-medium text-gray-700 mb-1">Organisation Unit</label> */}
+                                <Button disabled={isFetchCurrentInstanceDataItemsLoading || isFetchExternalInstanceDataItemsLoading} variant="source"  text={`${(isFetchCurrentInstanceDataItemsLoading || isFetchExternalInstanceDataItemsLoading ) ? "Loading.." : `Organisation Unit`} `} onClick={handleShowOrganizationUnitModal} />
                             </div>
                         </div>
                     </TabsContent>
