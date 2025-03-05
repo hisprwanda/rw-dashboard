04bc8910112466da15c74852e5cc40dd818f173c
NSENGIYUMVA-Christian <chris4dev7@gmail.com>
2025-03-05 15:57:15 +0200

    update example map data

--- src/pages/Map/MapComponent.tsx
+++ src/pages/Map/MapComponent.tsx
@@ -40,21 +40,56 @@ const MapComponent: React.FC = () => {
   const mapData = [
     { 
       id: '1', 
-      name: 'Health Facility A', 
-      latitude: -1.292066, 
-      longitude: 36.821946, 
-      value: 1500 
+      name: 'Kigali University Teaching Hospital (CHUK)', 
+      latitude: -1.9524, 
+      longitude: 30.0632, 
+      value: 2500 
     },
     { 
       id: '2', 
-      name: 'Health Facility B', 
-      latitude: -1.300577, 
-      longitude: 36.782575, 
-      value: 2300 
+      name: 'King Faisal Hospital', 
+      latitude: -1.9403, 
+      longitude: 30.0578, 
+      value: 2200 
+    },
+    { 
+      id: '3', 
+      name: 'Butaro District Hospital', 
+      latitude: -1.3854, 
+      longitude: 30.0515, 
+      value: 1800 
+    },
+    { 
+      id: '4', 
+      name: 'Gihundwe District Hospital', 
+      latitude: -2.2158, 
+      longitude: 29.2875, 
+      value: 1600 
+    },
+    { 
+      id: '5', 
+      name: 'Ruhengeri Hospital', 
+      latitude: -1.5044, 
+      longitude: 29.6339, 
+      value: 2000 
+    },
+    { 
+      id: '6', 
+      name: 'Kibungo District Hospital', 
+      latitude: -2.1392, 
+      longitude: 30.5424, 
+      value: 1700 
+    },
+    { 
+      id: '7', 
+      name: 'Muhima District Hospital', 
+      latitude: -1.9417, 
+      longitude: 30.0619, 
+      value: 1900 
     }
   ];
 
-  const centerPosition: [number, number] = [-1.292066, 36.821946];
+  const centerPosition: [number, number] = [-1.9403, 30.0578]; 
 
   return (
     <div className="flex h-screen">
