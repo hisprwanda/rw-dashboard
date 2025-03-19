736ba9fcbb8c3d12deb90ffd09d075cbbb17f60b
NSENGIYUMVA-Christian <chris4dev7@gmail.com>
2025-03-19 10:25:03 +0200

    done with basic map trypes

--- src/types/mapTypes.ts
+++ src/types/mapTypes.ts
@@ -1,4 +1,4 @@
-import { nullable, z } from "zod";
+import { z } from "zod";
 
 // Schema definition using zod
 export const MapDataSchema = z.object({
@@ -6,11 +6,23 @@ export const MapDataSchema = z.object({
   mapType: z.enum(["Thematic"]),
   mapName: z.string().nonempty({ message: "Map name is required" }),
   description: z.string().optional(),
-  query: z
-    .record(z.string(), z.any())
-    .refine((data) => Object.keys(data).length > 0, {
-      message: "Query object cannot be empty",
-    }), 
+  queries: z.object({
+    mapAnalyticsQueryOne: z
+      .record(z.string(), z.any())
+      .refine((data) => Object.keys(data).length > 0, {
+        message: "mapAnalyticsQueryOne object cannot be empty",
+      }),
+    mapAnalyticsQueryTwo: z
+      .record(z.string(), z.any())
+      .refine((data) => Object.keys(data).length > 0, {
+        message: "mapAnalyticsQueryTwo object cannot be empty",
+      }),
+    geoFeaturesQuery: z
+      .record(z.string(), z.any())
+      .refine((data) => Object.keys(data).length > 0, {
+        message: "geoFeaturesQuery object cannot be empty",
+      }),
+  }),
   dataSourceId: z.string().nonempty({ message: "Data source ID is required" }),
   createdBy: z.object({
     name: z.string(),
@@ -22,10 +34,11 @@ export const MapDataSchema = z.object({
     name: z.string(),
     id: z.string(),
   }),
-  organizationTree: z.array(z.string())?.optional(),
-  selectedOrgUnitLevel: z.array(z.number())?.optional(),
+  organizationTree: z.array(z.string()).optional(),
+  selectedOrgUnitLevel: z.array(z.number()).optional(),
 });
+
 // Infer form fields from the schema
 export type MapDataFormFields = z.infer<typeof MapDataSchema>;
 
-export type mapTypes =  "Thematic"
+export type mapTypes = "Thematic";
