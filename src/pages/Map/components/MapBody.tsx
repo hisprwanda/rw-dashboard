0d45a89993cc1c28fe262c4e97724ed19cd388de
NSENGIYUMVA-Christian <chris4dev7@gmail.com>
2025-03-28 09:24:48 +0200

    changed height of mapbody to 100%

--- src/pages/Map/components/MapBody.tsx
+++ src/pages/Map/components/MapBody.tsx
@@ -46,7 +46,7 @@ const MapBody: React.FC<MapBodyProps> = ({
   metaMapData,
   singleSavedMapData,
   mapId,
-  isHideSideBar
+  isHideSideBar 
 }) => {
   // State management
   const [currentBasemap, setCurrentBasemap] = useState<BasemapType>('osm-light');
@@ -147,7 +147,7 @@ const MapBody: React.FC<MapBodyProps> = ({
   };
 
   return (
-    <div className="flex h-screen">
+    <div className="flex flex-1 h-full w-full bg-red-500">
       {/* Sidebar */}
       {!isHideSideBar && <>
         <MapSidebar 
