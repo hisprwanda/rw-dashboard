3c997563f912c8eb64c7e57829b16bc9d77eb7e4
NSENGIYUMVA-Christian <chris4dev7@gmail.com>
2025-04-18 08:02:14 +0200

    refine the layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,12 +1,13 @@
-
 import { Outlet } from 'react-router-dom';
 import Header from '../header/header';
 
 export default function MainLayout() {
   return (
-    <div>
-      <div>
+    <div className="flex flex-col h-screen">
+      <div className="sticky top-0 z-10">
         <Header />
+      </div>
+      <div className="flex-1 overflow-auto">
         <Outlet />
       </div>
     </div>
